e5016aa5dc86d144c9786635c32ccd1b8d392e36
Egor <zangedzu333gig@gmail.com>
2018-09-20 11:18:06 +0300

    Modifed and modern client-qa

--- client-qa.js
+++ client-qa.js
@@ -5,19 +5,14 @@ const port = 8124;
 
 const client = new net.Socket();
 
-let ARRQ;
+let ARRQ = shuffle(require('./qa.json'));
 let CURRENTID = -1;
 
 client.setEncoding('utf8');
-client.connect(port, () => {
-	console.log('Connected');
-  	fs.readFile('qa.json', (e, text) => {
-        if (e) console.log(e);
-        else {
-            ARRQ = shuffle(JSON.parse(text));
-            client.write('QA');
-        }
-    });
+client.connect(port, () => { 
+	console.log(`X = ${process.env.X}`);
+	console.log('Connected'); 
+	client.write('QA');
 });
 
 client.on('data', (data) => {
@@ -25,11 +20,9 @@ client.on('data', (data) => {
 	if (data === 'DEC') client.destroy();
 	else if (data === 'ACK') sendQuestion();
 	else { 
-		let ANSW = 'Bad answer';
-		if (data === '1') ANSW = ARRQ[CURRENTID].g;
 		console.log('Question: ' + ARRQ[CURRENTID].q);
         console.log('Answer: ' + ARRQ[CURRENTID].g);
-        console.log('Server Answer: ' + ANSW);
+        console.log(`Server answer: ${data}`);
         sendQuestion();
 	}
 });
@@ -39,8 +32,7 @@ client.on('close', () => console.log('Connection closed'));
 function shuffle(array) {
 	let n;
 	var result = [];
-	while (array.length > 0)
-	{
+	while (array.length > 0) {
 		n = Math.floor(Math.random() * array.length);
 		result.push(array[n]);
 		array.splice(n, 1);

515408ff2ec3a941d9806e8ab635206daf1ac5e6
Egor <zangedzu333gig@gmail.com>
2018-09-22 10:41:33 +0300

    Create server login

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const net = require('net');
 const fs = require('fs');
 const port = 8124;
 let seed = 0;
+let targ = 0;
+let ARRQ = require('./qa.json');
+let FILES = [];
 let connections = 0;
 const log = fs.createWriteStream('client_id.txt');
 
@@ -19,15 +22,49 @@ const server = net.createServer((client) => {
 	log.write('[' + formatDate() + ']: ' +'Client #' + client.id + ' connected\n');
 
 	client.on('data', (data) => {
-		if (data === 'QA') client.write('ACK');
-        else {
-        	let answr = Math.floor(Math.random() * 2).toString();
+		if ((data === 'NEXT') || (data === 'QA')) {
+			if (data === 'NEXT') {
+				FILES[client.id] = [];
+                fs.mkdir(DEFAULT_DIR + path.sep + client.id);
+			}
+		}
+        else if (client.id === undefined) {
+            client.write(resBad);
+            client.destroy();
+        }
+
+        if (clients[client.id] === 'QA' && data !== 'QA') {     	
+		    let answr = "Bad answer";
+		    if (Math.floor(Math.random() * 2) === 1) {
+		    	let QID = -1;
+	        	for (let i = 0; i < ARRQ.length; i++)
+			        if (arr[i].question === data) {
+			        	QID = i;
+			        	break;
+			        }
+		    	answr = ARRQ[QID].g;
+		    }
         	log.write('[' + formatDate() + '][#' + client.id + '] > Data: ' + data + '; Answer: ' + answr + '\n');
 	        client.write(answr);	
-	    }
+	    } else if (clients[client.id] === 'FILES' && data !== 'FILES') {
+            FILES[client.id].push(data);
+            targ++;
+            if (targ === 2) {
+                let buf = Buffer.from(files[client.id][0], 'hex');
+                let filePath = defaultDir + path.sep + client.id + path.sep + files[client.id][1];
+                //console.log(filePath);
+                let fr = fs.createWriteStream(filePath);
+                fr.write(buf);
+                targ = 0;
+                files[client.id] = [];
+                fr.close();
+                client.write('FILES');
+            }
+        }
 	});
 
 	client.on('end', () => {
+		connections--;
 		console.log('[' + formatDate() + ']: ' +'Client #' + client.id + ' disconnected\n');
 		log.write('[' + formatDate() + ']: ' +'Client #' + client.id + ' disconnected\n');
 	});
